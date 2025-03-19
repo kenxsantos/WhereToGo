31e32afa0f550141585e4533f6ab58039b9a10b8
kenxsantos <kenxsantos@gmail.com>
2025-03-19 16:51:16 +0800

    add PostViewModel

--- app/src/main/java/com/example/wheretogo/PostViewModel.kt
+++ app/src/main/java/com/example/wheretogo/PostViewModel.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateListOf
 
 class PostViewModel : ViewModel() {
+
     private val _posts = mutableStateListOf<Post>()
-    val posts: List<Post> = _posts
+    val posts: List<Post> get()  = _posts
 
     fun addPost(name: String, origin: String, destination: String, description: String) {
         val newPost = Post(id = _posts.size + 1, name = name, origin = origin, destination = destination, description = description)
